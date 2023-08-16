fe7b8a338b370d748f32773e1da676f160a6f918
MartaCzM <>
2023-08-16 01:27:37 +0000

    Creating the main loop gto the game run

--- run.py
+++ run.py
@@ -43,3 +43,60 @@ def get_user_guess(size):
             continue
 
         return row, col
+
+def computer_guess(size):
+    """
+    This code introduces the computer_guess function, which generates a random guess for the computer.
+    After the user's guess, the computer's turn is triggered. 
+    """
+    guess_row = random.randint(0, size - 1)
+    guess_col = random.randint(0, size - 1)
+    return guess_row, guess_col
+
+def play_game():
+    """
+    This code represents the main loop of the game.
+    """
+    grid_size = 5
+    num_ships = 3
+
+    user_grid = create_grid(grid_size)
+    computer_grid = create_grid(grid_size)
+
+    place_ships(computer_grid, num_ships)
+
+    print("Welcome to Battleships!")
+    print("Guess the positions of the computer's ships.")
+
+    while True:
+        print("Your grid:")
+        print_grid(user_grid)
+
+        user_row, user_col = get_user_guess(grid_size)
+
+        if computer_grid[user_row][user_col] == 'S':
+            print("Congratulations! You hit a ship!")
+            user_grid[user_row][user_col] = 'X'
+        else:
+            print("Sorry, it's a miss.")
+            user_grid[user_row][user_col] = 'O'
+
+        if all(cell == 'X' for row in user_grid for cell in row):
+            print("Congratulations! You sank all the ships!")
+            break
+
+        print("Computer's turn:")
+        computer_row, computer_col = computer_guess(grid_size)
+
+        if user_grid[computer_row][computer_col] == 'S':
+            print("Oh no! The computer hit your ship!")
+            user_grid[computer_row][computer_col] = 'X'
+        else:
+            print("Phew! The computer missed.")
+            user_grid[computer_row][computer_col] = 'O'
+
+        if all(cell == 'X' for row in user_grid for cell in row):
+            print("Oh no! The computer sank all your ships!")
+            break
+
+play_game()
