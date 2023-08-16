1f060d96822b73086e38b4a224965b44ee11bdd2
MartaCzM <>
2023-08-16 01:19:00 +0000

    Defining the get user guesses function

--- run.py
+++ run.py
@@ -24,4 +24,22 @@ def place_ships(grid, num_ships):
             ship_col = random.randint(0, len(grid[0]) - 1)
         grid[ship_row][ship_col] = 'S'
 
-        
+
+def get_user_guess(size):
+    """
+    Function that will make the player guesses possible
+    """
+    while True:
+        guess = input(f"Enter your guess (e.g., A3): ").strip().upper()
+        if len(guess) != 2 or not guess[0].isalpha() or not guess[1].isdigit():
+            print("Invalid input. Please use the format: LetterNumber (e.g., A3)")
+            continue
+
+        row = ord(guess[0]) - ord('A')
+        col = int(guess[1]) - 1
+
+        if row < 0 or row >= size or col < 0 or col >= size:
+            print("Guess is off-grid. Try again.")
+            continue
+
+        return row, col
